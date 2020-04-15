1521f456007e3d17473d351d93408b230a4dc00e
Looqer <lukaszsienkiewicz94@gmail.com>
2020-04-15 22:33:52 +0200

    first python commit

---
+++ PythonLearningProject.py
@@ -0,0 +1,27 @@
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Sum
+sum = float(num1) + float(num2)
+
+# Difference
+
+
+# Product
+
+
+# Quotient
+
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+# Display the difference
+
+
+# Display the product
+
+
+# Display the quotient
+
